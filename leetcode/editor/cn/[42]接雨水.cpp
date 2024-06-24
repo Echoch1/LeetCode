1f11bbc31f05df1dd32d7b70c40b0176abd2e95f
Echoch1 <852573130@qq.com>
2024-06-24 23:28:36 +0800

    062406240624062406240624

--- leetcode/editor/cn/[42]接雨水.cpp
+++ leetcode/editor/cn/[42]接雨水.cpp
@@ -5,7 +5,19 @@ using namespace std;
 class Solution {
 public:
     int trap(vector<int>& height) {
-
+        int res = 0;
+        stack<int> stack1;
+        for (int i = 0; i < height.size(); i++) {
+            while (!stack1.empty() && height[i] >= height[stack1.top()]) {
+                int h = height[stack1.top()];
+                stack1.pop();
+                if (!stack1.empty()) {
+                    res += (i - stack1.top() - 1) * (min(height[i], height[stack1.top()]) - h);
+                }
+            }
+            stack1.push(i);
+        }
+        return res;
     }
 };
 //leetcode submit region end(Prohibit modification and deletion)
@@ -13,4 +25,6 @@ public:
 
 int main() {
     Solution s;
+    vector<int> nums = {5, 4, 5};
+    cout << s.trap(nums) << endl;
 }
