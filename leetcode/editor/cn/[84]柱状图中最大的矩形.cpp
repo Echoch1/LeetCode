8afea6c452f88c1df1a5a04e853c9edb4b0e5d91
Echoch1 <852573130@qq.com>
2024-06-25 00:16:05 +0800

    0624

---
+++ leetcode/editor/cn/[84]柱状图中最大的矩形.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        int res = 0;
+        stack<int> stack1;
+        for (int i = 0; i < heights.size(); i++) {
+            
+        }
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+int main()
+{
+    Solution s;
+}
