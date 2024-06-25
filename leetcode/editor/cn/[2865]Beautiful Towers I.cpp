0c594b00bafa7be99ed70dca5d17f877c05480e2
chenchaohao <852573130@qq.com>
2024-06-25 17:55:40 +0800

    0625

--- leetcode/editor/cn/[2865]Beautiful Towers I.cpp
+++ leetcode/editor/cn/[2865]Beautiful Towers I.cpp
@@ -5,7 +5,37 @@ using namespace std;
 class Solution {
 public:
     long long maximumSumOfHeights(vector<int>& heights) {
-        
+        long long res = 0;
+        stack<int> stack1;
+        vector<long long> pre;
+        vector<long long> suf;
+        long long sum = 0;
+        for (int i = heights.size() - 1; i >= 0; i--) {
+            while (!stack1.empty() && heights[i] <= heights[stack1.top()]) {
+                stack1.pop();
+                sum -= heights[stack1.top()] * (heights.size() - stack1.top());
+            }
+            stack1.push(i);
+            sum += heights[i] * (heights.size() - i);
+            suf.push_back(sum);
+        }
+        while(!stack1.empty()) {
+            stack1.pop();
+        }
+        sum = 0;
+        for (int i = 0; i < heights.size(); i++) {
+            while (!stack1.empty() && heights[i] <= heights[stack1.top()]) {
+                stack1.pop();
+                sum -= heights[stack1.top()] * (heights.size() - stack1.top());
+            }
+            stack1.push(i);
+            sum += heights[i] * (heights.size() - i);
+            pre.push_back(sum);
+        }
+        for (int i = 0; i < pre.size(); i++) {
+            res = max(res, pre[i] + suf[i]);
+        }
+        return res;
     }
 };
 //leetcode submit region end(Prohibit modification and deletion)
