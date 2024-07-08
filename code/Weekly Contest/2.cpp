6dc85b0e295f85a3d75a6f41be8e1904b87b6cf1
chenchaohao <852573130@qq.com>
2024-07-08 15:05:44 +0800

    weekly

--- code/Weekly Contest/2.cpp
+++ code/Weekly Contest/2.cpp
@@ -4,11 +4,16 @@ using namespace std;
 class Solution {
 public:
     vector<string> validStrings(int n) {
-        if (n == 1) {
-            return { "0" , "1" };
-        }
         vector<string> res;
-
+        int m = (1 << n) - 1;
+        for (int i = 0; i <= m; ++i) {
+            int j = i ^ m;
+            if ((j & (j >> 1)) == 0) {
+                bitset<18> bs(i);
+                res.push_back(bs.to_string().substr(18 - n));
+            }
+        }
+        return res;
     }
 };
 
